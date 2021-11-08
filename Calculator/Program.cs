db2671f23442d37616eacb1c5d6bcc6ec36bb16b
artj15 <artj15@gmail.com>
2021-11-08 20:52:31 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            float b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите оператор: /, *, -, +.");
+            string c = Console.ReadLine();
+            if (c == char.ToString('/') & b != 0)
+            {
+                Console.WriteLine(a / b);
+            }
+            else if (c == char.ToString('*'))
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (c == char.ToString('-'))
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (c == char.ToString('+'))
+            {
+                Console.WriteLine(a + b);
+            };
+            Console.ReadKey();
+        }
+    }
+}
