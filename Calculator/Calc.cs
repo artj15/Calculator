a53c868fbec91b3de62c3875c894c55e6f0310fa
artj15 <artj15@gmail.com>
2021-11-16 12:51:43 +0300

    Tests fixed.

---
+++ Calculator/Calc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calc
+    {
+
+        public int num1 = 0; public int num2 = 0;
+       
+        public static void Main(string[] args)
+        {
+            var mc = new Calc();
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            mc.num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            mc.num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {mc.num1} + {mc.num2} = " + (mc.Summ(mc.num1, mc.num2)));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {mc.num1} - {mc.num2} = " + (mc.Sub(mc.num1, mc.num2)));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {mc.num1} * {mc.num2} = " + (mc.Mult(mc.num1, mc.num2)));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {mc.num1} / {mc.num2} = " + (mc.Div(mc.num1, mc.num2)));
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            //Console.Write("Press any key to close the Calculator console app...");
+            //Console.ReadKey();
+        }
+        public int Summ(int num1, int num2)
+        {
+            var add = num1 + num2;
+            return add;
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            var sub = num1 - num2;
+            return sub;
+        }
+        public int Mult(int num1, int num2)
+        {
+            var mul = num1 * num2;
+            return mul;
+        }
+        public int Div(int num1, int num2)
+        {
+            var div = num1 / num2;
+            return div;
+        }
+    }
+}
---
+++ test/Calculator.Tests/CalcTest.cs
@@ -0,0 +1,106 @@
+using System;
+using System.IO;
+using Xunit;
+using System.Threading;
+using System.Globalization;
+
+namespace Calculator.Tests
+{
+    public class CalcTest
+    {
+        [Fact]
+        public void ShowBeAbleToCreateCalc()
+        {
+            Calc calc = new Calc();
+            calc.num1 = 2;
+            calc.num2 = 3;
+            Assert.Equal(5, calc.Summ(calc.num1, calc.num2));
+            Assert.Equal(-1, calc.Sub(calc.num1, calc.num2));
+            Assert.Equal(6, calc.Mult(calc.num1, calc.num2));
+            Assert.Equal(2 / 3, calc.Div(calc.num1, calc.num2));
+            Assert.NotNull(calc);
+        }
+
+        [Fact]
+        public void ShowBeAbleToSum()
+        {
+            var stingWriter = new StringWriter();
+            Console.SetOut(stingWriter);
+            var userIn = new StringReader("12" + Environment.NewLine + '3' + Environment.NewLine + 'a' + Environment.NewLine + 'b');
+            Console.SetIn(userIn);
+            Calc.Main(new string[] { });
+            var expectedOutput = "Console Calculator in C#\r\r\n" +
+                                 "------------------------\n\r\n" +
+                                 "Type a number, and then press Enter\r\n" +
+                                 "Type another number, and then press Enter\r\n" +
+                                 "Choose an option from the following list:\r\n\t" +
+                                 "a - Add\r\n\t" +
+                                 "s - Subtract\r\n\t" +
+                                 "m - Multiply\r\n\t" +
+                                 "d - Divide\r\n" +
+                                 "Your option? Your result: 12 + 3 = 15\r\n";
+            Assert.Equal(expectedOutput, stingWriter.ToString());
+        }
+
+        [Fact]
+        public void ShowBeAbleToSub()
+        {
+            var stingWriter = new StringWriter();
+            Console.SetOut(stingWriter);
+            var userIn = new StringReader("12" + Environment.NewLine + '3' + Environment.NewLine + 's' + Environment.NewLine + 'b');
+            Console.SetIn(userIn);
+            Calc.Main(new string[] { });
+            var expectedOutput = "Console Calculator in C#\r\r\n" +
+                                 "------------------------\n\r\n" +
+                                 "Type a number, and then press Enter\r\n" +
+                                 "Type another number, and then press Enter\r\n" +
+                                 "Choose an option from the following list:\r\n\t" +
+                                 "a - Add\r\n\t" +
+                                 "s - Subtract\r\n\t" +
+                                 "m - Multiply\r\n\t" +
+                                 "d - Divide\r\n" +
+                                 "Your option? Your result: 12 - 3 = 9\r\n";
+            Assert.Equal(expectedOutput, stingWriter.ToString());
+        }
+        [Fact]
+        public void ShowBeAbleToMult()
+        {
+            var stingWriter = new StringWriter();
+            Console.SetOut(stingWriter);
+            var userIn = new StringReader("12" + Environment.NewLine + '3' + Environment.NewLine + 'm' + Environment.NewLine + 'b');
+            Console.SetIn(userIn);
+            Calc.Main(new string[] { });
+            var expectedOutput = "Console Calculator in C#\r\r\n" +
+                                 "------------------------\n\r\n" +
+                                 "Type a number, and then press Enter\r\n" +
+                                 "Type another number, and then press Enter\r\n" +
+                                 "Choose an option from the following list:\r\n\t" +
+                                 "a - Add\r\n\t" +
+                                 "s - Subtract\r\n\t" +
+                                 "m - Multiply\r\n\t" +
+                                 "d - Divide\r\n" +
+                                 "Your option? Your result: 12 * 3 = 36\r\n";
+            Assert.Equal(expectedOutput, stingWriter.ToString());
+        }
+        [Fact]
+        public void ShowBeAbleToDiv()
+        {
+            var stingWriter = new StringWriter();
+            Console.SetOut(stingWriter);
+            var userIn = new StringReader("12" + Environment.NewLine + '3' + Environment.NewLine + 'd' + Environment.NewLine + 'b');
+            Console.SetIn(userIn);
+            Calc.Main(new string[] { });
+            var expectedOutput = "Console Calculator in C#\r\r\n" +
+                                 "------------------------\n\r\n" +
+                                 "Type a number, and then press Enter\r\n" +
+                                 "Type another number, and then press Enter\r\n" +
+                                 "Choose an option from the following list:\r\n\t" +
+                                 "a - Add\r\n\t" +
+                                 "s - Subtract\r\n\t" +
+                                 "m - Multiply\r\n\t" +
+                                 "d - Divide\r\n" +
+                                 "Your option? Your result: 12 / 3 = 4\r\n";
+            Assert.Equal(expectedOutput, stingWriter.ToString());
+        }
+    }
+}
